b50544f06f0d8c001cc2dfd20dc9cb5c43535e99
i343281 <samuel.davies@sap.com>
2025-02-22 12:28:12 +0000

    feat(storage): implement storage for posts and users

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,15 +5,18 @@ import (
 	"os"
 
 	"github.com/draysams/gosocial/internal/env"
+	"github.com/draysams/gosocial/internal/store"
 )
 
 func main() {
 	cfg := config{
 		addr: env.GetString("PORT", ":8080"),
 	}
+	store := store.NewStorage(nil)
 
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
 
 	os.LookupEnv("PORT")
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/draysams/gosocial
 
 go 1.23.1
 
-require github.com/go-chi/chi/v5 v5.2.1 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
